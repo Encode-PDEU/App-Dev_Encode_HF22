32bb4bf1ea8a2ac1781700c1912f0faa66e980ed
Kishan Pipariya <intelkishan@gmail.com>
2022-10-23 22:38:36 +0530

    Added a function to generate game tile instead of manual creation

--- quizzler/lib/screens/quiz.dart
+++ quizzler/lib/screens/quiz.dart
@@ -8,6 +8,45 @@ class QuizPages extends StatefulWidget {
 }
 
 class _QuizPagesState extends State<QuizPages> {
+  Widget gameTile(String field1, String field2, String field3) {
+    return Container(
+      height: 170,
+      width: 200,
+      margin: const EdgeInsets.symmetric(horizontal: 40, vertical: 30),
+      decoration: BoxDecoration(
+        color: Colors.lightBlue.shade100,
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Text(
+            field1,
+            style: const TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(field2,
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 15,
+              )),
+          ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                shape: const StadiumBorder(),
+                minimumSize: const Size(110, 30),
+                maximumSize: const Size(120, 70),
+                backgroundColor: Colors.white,
+                foregroundColor: Colors.black,
+              ),
+              child: Text(field3)),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,47 +62,12 @@ class _QuizPagesState extends State<QuizPages> {
       ),
       body: ListView(
         children: [
-          Container(
-            height: 170,
-            width: 200,
-            margin: EdgeInsets.symmetric(horizontal: 40, vertical: 30),
-            decoration: BoxDecoration(
-              color: Colors.lightBlue.shade100,
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  child: const Text(
-                    'Create a Quiz',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                Container(
-                  child: const Text(
-                      'Create your own Quiz \n Play with your friends',
-                      style: TextStyle(
-                        fontSize: 15,
-                      )),
-                ),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Create'),
-                  style: ElevatedButton.styleFrom(
-                    shape: StadiumBorder(),
-                    minimumSize: Size(110, 30),
-                    maximumSize: Size(120, 70),
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
-                  ),
-                ),
-              ],
-            ),
-          )
+          gameTile('Create a Quiz',
+              'Create your own Quiz \n Play with your friends', 'Create'),
+          gameTile(
+              'Play With Friends',
+              'Invite your fiends \n Compete and win together',
+              'Invite Friends'),
         ],
       ),
     );
