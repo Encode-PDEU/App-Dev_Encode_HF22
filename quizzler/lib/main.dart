7ff31a644224584a29ec000f59047f304802da71
Kishan Pipariya <intelkishan@gmail.com>
2022-10-26 21:51:52 +0530

    Added bottom navigation bar

--- quizzler/lib/main.dart
+++ quizzler/lib/main.dart
@@ -2,23 +2,70 @@ import 'package:flutter/material.dart';
 import 'quiz_brain.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'screens/quiz.dart';
+
 QuizBrain questionBank = QuizBrain();
+int selectedIndex=0;
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MyApp();
+  }
+}
+
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+
+class _MyAppState extends State<MyApp> {
+
+  void onItemTapped(int index){
+    setState(() {
+      selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData.light().copyWith(
-            colorScheme: ColorScheme.fromSwatch().copyWith(
-              primary: Color(0xFF3F51B5),
-            ),
-            scaffoldBackgroundColor: const Color(0xFF3F51B5)
+      theme: ThemeData.light().copyWith(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: Color(0xFF3F51B5),
+          ),
+          scaffoldBackgroundColor: const Color(0xFF3F51B5)),
+      home: SafeArea(
+        child: Scaffold(
+          body: QuizPages(),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.lightbulb_outline_sharp),
+                label: 'Quizzing',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline),
+                label: 'Profile',
+              ),
+            ],
+            currentIndex: selectedIndex,
+            onTap: onItemTapped,
+          ),
         ),
-      home: QuizPages()
+      ),
     );
   }
 }
@@ -35,13 +82,21 @@ class _QuizPageState extends State<QuizPage> {
 
   void checkAnswer(bool userAnswer) {
     setState(() {
-      if(questionBank.getAnswer() == userAnswer)
-        scoreKeeper.add(Icon(Icons.check, color: Colors.green,));
+      if (questionBank.getAnswer() == userAnswer)
+        scoreKeeper.add(Icon(
+          Icons.check,
+          color: Colors.green,
+        ));
       else
-        scoreKeeper.add(Icon(Icons.close, color: Colors.red,));
-      if(questionBank.questionNumber == questionBank.getLength()-1)
+        scoreKeeper.add(Icon(
+          Icons.close,
+          color: Colors.red,
+        ));
+      if (questionBank.questionNumber == questionBank.getLength() - 1)
         Alert(
-          context: context, type: AlertType.error, title: "THE END",
+          context: context,
+          type: AlertType.error,
+          title: "THE END",
           desc: "You've completed the quiz",
           buttons: [
             DialogButton(
@@ -60,7 +115,8 @@ class _QuizPageState extends State<QuizPage> {
             )
           ],
         ).show();
-      else questionBank.getNextQuestion();
+      else
+        questionBank.getNextQuestion();
     });
   }
 
@@ -90,7 +146,10 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15),
             child: TextButton(
-              child: Text('True', style: TextStyle(fontSize: 20, color: Colors.white),),
+              child: Text(
+                'True',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
               style: TextButton.styleFrom(
                 backgroundColor: Colors.green,
               ),
@@ -104,9 +163,12 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15),
             child: TextButton(
-              child: Text('False', style: TextStyle(fontSize: 20, color: Colors.white),),
+              child: Text(
+                'False',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
               style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
+                backgroundColor: Colors.red,
               ),
               onPressed: () {
                 checkAnswer(false);
@@ -119,4 +181,3 @@ class _QuizPageState extends State<QuizPage> {
     );
   }
 }
-
